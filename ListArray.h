67a8806405d020ff2bd0a11ebb417bfb3b08b64e
alejandroferrando <alejandrolechi@gmail.com>
2025-11-13 12:32:47 +0100

    Refactor List class to ListArray with dynamic array

---
+++ ListArray.h
@@ -0,0 +1,96 @@
+#include <ostream>
+#include "List.h"
+using namespace std;
+template <typename T>
+class ListArray : public List<T> {
+    private:
+        T* arr;
+        int max;
+        int n;
+        static const int MINSIZE = 2;
+    public:
+        ListArray();
+        ~ListArray();
+
+        void resize (int new_size) override{
+			T* new_arr = new T[new_size];
+			for(int i=0;i<n;i++){
+				new_arr[i] = arr[i];
+			}
+			delete[] arr;
+			T* arr = new_arr;
+			max = new_size;
+		}
+        int size()override{
+			return n;
+		}
+        void insert(int pos, T element)override{
+			if(pos<0||pos>size()-1){
+				throw out_of_range("Posicion no valida");
+			}
+			n++;
+			if(n>max){
+				resize(n+1);
+			}
+			for(int i=n;i>=pos;i--){
+			arr[i]=arr[i-1];
+			}
+			arr[pos] = element;
+		}
+        void append(T element)override{
+			insert(n, element);
+		}
+
+        void prepend(T element)override{
+			insert(0, element);
+		}
+        T remove(int pos)override{
+			if(pos<0||pos>size()-1){	
+				throw out_of_range("Posicion no valida");
+			}
+			int aux = arr[pos];
+			for(int i=pos;i<n;i++){
+				arr[i] = arr[i+1];
+			}
+			return aux;
+		}
+        T get(int pos)override{
+			if(pos<0 || pos>size()-1){
+				throw out_of_range("Posicion no valida");
+			}
+			return arr[pos];
+		}
+        T operator[](int pos)override{
+			return get(pos);
+        }
+        bool empty()override{
+		    if(n!=0){
+		       		return true;
+		 	}
+            else{
+                return false;
+            }
+		}
+        friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
+			out<<"List --> [";
+			for(int i=0; i<list.n;i++){
+				out<<list.arr[i];
+			}
+			out<<"]\n";
+			return out;
+		}
+        int search(T element)override{
+			for(int i=0;i<n;i++){
+				if(arr[i]==element){
+					return i;
+				}
+			}
+			return -1;
+		}
+};
+    
+        
+        
+
+        
+			
